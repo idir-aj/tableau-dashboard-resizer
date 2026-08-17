baccb1baa714e91d305cba437c334db408596dad
Idir S. <96688331+idir-aj@users.noreply.github.com>
2026-08-17 14:15:55 +0200

    Update app_resize.py

--- app_resize.py
+++ app_resize.py
@@ -15,7 +15,7 @@ from utils import (
     parser_xml,
     remballer_twbx,
 )
-from outil_resize import (
+from resize_engine import (
     recuperer_dashboards_avec_tailles,
     modifier_tableaux_de_bord,
     init_df_resize,
